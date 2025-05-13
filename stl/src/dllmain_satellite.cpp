4a01d6e265e95c36d52a6add34ef55eb53fad953
fangrong <darkbluesky@outlook.com>
2025-05-13 18:47:58 +0800

    dllmain_satellite: Added NO_SUBGROUP_GUID/GUID_NULL

--- stl/src/dllmain_satellite.cpp
+++ stl/src/dllmain_satellite.cpp
@@ -3,6 +3,11 @@
 
 #include <Windows.h>
 
+#include <initguid.h>
+
+DEFINE_GUID(NO_SUBGROUP_GUID, 0xfea3413e, 0x7e05, 0x4911, 0x9a, 0x71, 0x70, 0x03, 0x31, 0xf1, 0xc2, 0x94);
+DEFINE_GUID(GUID_NULL, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+
 // DllMain for 'satellite' DLLs which don't need TLS.
 
 extern "C" BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID) noexcept {
