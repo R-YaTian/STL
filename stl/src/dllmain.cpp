5fe66b51ed9a84278042dc4e85dd80cc3f4b8432
fangrong <darkbluesky@outlook.com>
2025-05-13 16:18:21 +0800

    dllmain: Added NO_SUBGROUP_GUID/GUID_NULL

--- stl/src/dllmain.cpp
+++ stl/src/dllmain.cpp
@@ -5,6 +5,11 @@
 
 #include <Windows.h>
 
+#include <initguid.h>
+
+DEFINE_GUID(NO_SUBGROUP_GUID, 0xfea3413e, 0x7e05, 0x4911, 0x9a, 0x71, 0x70, 0x03, 0x31, 0xf1, 0xc2, 0x94);
+DEFINE_GUID(GUID_NULL, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+
 extern "C" BOOL APIENTRY DllMain(HMODULE, DWORD, LPVOID) noexcept {
     return TRUE;
 }
